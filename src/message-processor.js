6d41a9ed7f7df2824febce02f4b9b0c095fe022c
porebrique <porebrique@gmail.com>
2017-11-21 17:57:37 +0300

    Bullshitter module: small improvements (#16)

--- src/message-processor.js
+++ src/message-processor.js
@@ -5,10 +5,29 @@ import { bind } from './utils';
 
 export default class MessageProcessor {
 
+  static isTimeToSayRandomly () {
+    const chance = settings.randomPhraseChance || 0;
+    const random = Math.floor(Math.random() * 100);
+    return chance > random;
+  }
+
+  /**
+   * Returns regexp matching any of bot's names followed by space, or comma, or end of string.
+   * @returns {RegExp}
+   */
+  static getBotMentionRegexp () {
+    const namesJoined = settings.names.map(name => `(${name})`).join('|');
+    return new RegExp(`^(${namesJoined})(\\s|,|$)`, 'i');
+  }
+
   constructor(bot) {
     this.bot = bot;
     this.bot.settings = settings;
     this.commandProcessor = new CommandProcessor(bot);
+    // Every message is being checked if it is said to bot.
+    // To avoid constant re-calculating, lets generate and store required regexp once.
+    this.bot.botMentionsRegexp = this.constructor.getBotMentionRegexp();
+    console.log('this.bot.botMentionsRegexp:', this.bot.botMentionsRegexp);
 
     bind([
       'processMessage',
@@ -52,30 +71,14 @@ export default class MessageProcessor {
     }
   }
 
-  /**
-   * Returns regexp matching any of bot's names followed by space, or comma, or end of string.
-   * Regexp is generated only once, then kept cached in this.bot.botMentionsRegexp
-   * @returns {RegExp}
-   */
-  getBotMentionRegexp () {
-    const result = this.bot.botMentionsRegexp;
-    if (!this.bot.botMentionsRegexp) {
-      const namesJoined = this.bot.settings.names.reduce((last, next) => {
-        return '(' + last + ')' + '|(' + next + ')';
-      });
-
-      this.bot.botMentionsRegexp = new RegExp('^(' + namesJoined + ')(\\s|,|$)', 'i');
-    }
-    return this.bot.botMentionsRegexp;
-  }
-
   sayRandomShit (msg) {
     const result = bullshitter.getBullshit(this.removeBotMentions(msg.text));
     return result || null;
   }
 
   removeBotMentions (messageText) {
-    return messageText.replace(this.getBotMentionRegexp(), '').trim();
+    const { botMentionsRegexp } = this.bot;
+    return messageText.replace(botMentionsRegexp, '').trim();
   }
 
 // TODO: somehow combine with removeBotMentions for avoiding second iteration
@@ -91,18 +94,13 @@ export default class MessageProcessor {
     return messageIsForBot;
   }
 
-  isTimeToSayRandomly () {
-    const chance = settings.randomPhraseChance || 0;
-    const random = Math.floor(Math.random() * 100);
-    return chance > random;
-  }
-
   processGeneralMessage (msg) {
     const isSaidToBot = this.isSaidToBot(msg.text);
-    const isTimeToSayRandomly = this.isTimeToSayRandomly();
-    const resultMessage = (isSaidToBot || isTimeToSayRandomly ) && this.sayRandomShit(msg);
+    const isTimeToSayRandomly = this.constructor.isTimeToSayRandomly();
+    const isReplyRequired = isSaidToBot || isTimeToSayRandomly;
 
-    if (resultMessage) {
+    if (isReplyRequired) {
+      const resultMessage = this.sayRandomShit(msg);
       this.sendMessage(msg.chat.id, resultMessage);
     }
   }

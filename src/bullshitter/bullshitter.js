0bd644637035283fa6e170b34b6871c0c580ad57
porebrique <porebrique@gmail.com>
2018-12-26 14:57:32 +0300

    fix

--- src/bullshitter/bullshitter.js
+++ src/bullshitter/bullshitter.js
@@ -1,38 +1,13 @@
 import Linguistics from './linguistics';
 import BullshitStorage from './storage/bullshit-storage';
-
-const removeWhitespacesBeforePunctuation = sString => sString.replace(/(\s)([,.?!]+)/g, '$2');
-const CapitalizeWholePhrase = sString => sString.charAt(0).toUpperCase() + sString.slice(1);
-const transformOutput = bullshitText => CapitalizeWholePhrase(removeWhitespacesBeforePunctuation(bullshitText));
+import {
+  extractStringsFromDBItems,
+  mergeSomethingFromArray,
+  mergeSomethingWith,
+  transformOutput
+} from "./utils";
 
 /* ------------------------------------*/
-
-const extractStringsFromDBItems = input => input.map(item => item.text);
-
-/*
-  Tries to merge some of provided phrases, returns null can't
-  @returns {String|null}
- */
-const mergeSomethingFromArray = (aStrings, word) => {
-  const mergeablePair = Linguistics.getPairToMerge(aStrings, word);
-  return mergeablePair && Linguistics.mergePair(mergeablePair, word);
-};
-
-/**
- * Tries to merge received phrase with some other from base.
- * If phrase does not have any matches to stored items by any word
- * @param input
- * @returns {String|null}
- */
-const mergeSomethingWith = input => {
-  const { matches, word } = Linguistics.getMatchesSet(input);
-  if (!matches.length) {
-    return null;
-  }
-  // TODO: Why adding input?
-  const extractedPhrases = extractStringsFromDBItems(matches).concat([input]);
-  return mergeSomethingFromArray(extractedPhrases, word);
-};
 
 const getBullshit = input => {
   const { matches, word } = Linguistics.getMatchesSet(input); // could be 0, 1 or many
